05e26ee793072e8e76c919193d250f8e08ee2580
Yitzchok (Adminjew) <adminjew@gmail.com>
2010-04-09 02:13:52 -0400

    Changing source headers to LGPL

--- src/Zmanim/util/Zman.cs
+++ src/Zmanim/util/Zman.cs
@@ -1,19 +1,22 @@
-﻿// * Zmanim Java API
+﻿// * Zmanim .NET API
 // * Copyright (C) 2004-2010 Eliyahu Hershfeld
 // *
 // * Converted to C# by AdminJew
 // *
-// * This program is free software; you can redistribute it and/or modify it under the terms of the
-// * GNU General Public License as published by the Free Software Foundation; either version 2 of the
-// * License, or (at your option) any later version.
+// * This file is part of Zmanim .NET API.
 // *
-// * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-// * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// * General Public License for more details.
+// * Zmanim .NET API is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU Lesser General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
 // *
-// * You should have received a copy of the GNU General Public License along with this program; if
-// * not, write to the Free Software Foundation, Inc. 59 Temple Place - Suite 330, Boston, MA
-// * 02111-1307, USA or connect to: http://www.fsf.org/copyleft/gpl.html
+// * Zmanim .NET API is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU Lesser General Public License for more details.
+// *
+// * You should have received a copy of the GNU Lesser General Public License
+// * along with Zmanim.NET API.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
 
 namespace net.sourceforge.zmanim.util
 {
