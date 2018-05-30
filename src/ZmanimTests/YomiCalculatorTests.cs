0b33af41bc17f8b23a61cfd07d2062fabc813fbf
Yitzchok Gottlieb <adminjew@gmail.com>
2018-05-29 23:46:14 -0400

    Add some more days to test Daf Yomi

--- src/ZmanimTests/YomiCalculatorTests.cs
+++ src/ZmanimTests/YomiCalculatorTests.cs
@@ -15,8 +15,12 @@ namespace ZmanimTests
         [TestCase(2017, 9, 30, Result = "Sanhedrin:76")]
         [TestCase(2017, 10, 1, Result = "Sanhedrin:77")]
         [TestCase(2017, 10, 17, Result = "Sanhedrin:93")]
+        [TestCase(2018, 5, 29, Result = "Zevachim:46")]
+        [TestCase(2019, 9, 19, Result = "Meilah:2")]
+        [TestCase(2019, 10, 10, Result = "Kinnim:23")]
         [TestCase(2019, 10, 20, Result = "Tamid:33")]
         [TestCase(2019, 10, 21, Result = "Midos:34")]
+        [TestCase(2019, 10, 25, Result = "Niddah:2")]
         public string Can_DafYomi_Returns_The_Correct_Daf(int year, int month, int day)
         {
             var yomi = YomiCalculator.GetDafYomiBavli(new DateTime(year, month, day));
