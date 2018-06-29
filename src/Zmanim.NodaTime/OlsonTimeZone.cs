5f24931ab5ca0686d1c06e80bd8f553692dbee76
Yitzchok Gottlieb <adminjew@gmail.com>
2018-06-29 01:06:42 -0400

    Use NodaTime instead of the PublicDomain version of Olson TzDatabase we were working off of before

---
+++ src/Zmanim.NodaTime/OlsonTimeZone.cs
@@ -0,0 +1,18 @@
+﻿using NodaTime;
+
+namespace Zmanim.TzDatebase
+{
+    /// <summary>
+    /// A ITimeZone implementation of the Olson TimeZone DataBase
+    /// </summary>
+    public class OlsonTimeZone : NodaTimeZone
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OlsonTimeZone"/> class.
+        /// </summary>
+        /// <param name="timeZoneName">Name of the time zone.</param>
+        public OlsonTimeZone(string timeZoneName)
+            : base(DateTimeZoneProviders.Tzdb[timeZoneName]) { }
+
+    }
+}
