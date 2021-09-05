434a38622c347e6b35d10bc375d05ce6144c6939
yitzchok <78982+Yitzchok@users.noreply.github.com>
2021-09-05 13:45:43 -0400

    Fix month used for IsKislevShort

--- src/ZmanimTests/ParshaTests.cs
+++ src/ZmanimTests/ParshaTests.cs
@@ -7,9 +7,9 @@ namespace ZmanimTests
     [TestFixture]
     class ParshaTests
     {
-
         [TestCase(2021, 1, 9, ExpectedResult = JewishCalendar.Parsha.SHEMOS, TestName = "Non-leap year")]
         [TestCase(2019, 5, 25, ExpectedResult = JewishCalendar.Parsha.BEHAR, TestName = "Leap year")]
+        [TestCase(2029, 1, 6, ExpectedResult = JewishCalendar.Parsha.SHEMOS, TestName = "Non-leap year Long Kislev")]
         public JewishCalendar.Parsha OutsideIsrael(int year, int month, int day)
         {
             var jewishCalendar = new JewishCalendar();
